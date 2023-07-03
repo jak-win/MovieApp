90adb756e75e134ec6fe08dcf4084607bfa7a6bc
jak-win <kuba358190000@gmail.com>
2023-07-03 14:13:02 +0200

    Initial commit

---
+++ app/src/main/java/pl/wincenciuk/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,50 @@
+package pl.wincenciuk.movieapp.screens.home
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import pl.wincenciuk.movieapp.model.Movie
+import pl.wincenciuk.movieapp.model.getMovies
+import pl.wincenciuk.movieapp.navigation.MovieScreens
+import pl.wincenciuk.movieapp.widgets.MovieRow
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.LightGray,
+            elevation = 0.dp) {
+            Text(text = "Movies")
+
+        }
+    }) {
+        MainContent(navController = navController)
+
+    }
+
+}
+@Composable
+fun MainContent(
+    navController: NavController,
+    movieList: List<Movie> = getMovies()) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it){movie ->
+                    navController.navigate(route = MovieScreens.DetailsScreen.name+"/$movie")
+                }
+
+            }
+        }
+    }
+}
